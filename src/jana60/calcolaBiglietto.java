07e5bea47d63bec57865380bd93af1aa45b57d6f
Pier95F <forcinapierpaolo@gmail.com>
2022-06-07 15:41:58 +0200

    esercizio terminato

--- src/jana60/calcolaBiglietto.java
+++ src/jana60/calcolaBiglietto.java
@@ -1,10 +1,32 @@
 package jana60;
 
+import java.util.Scanner;
+
 public class calcolaBiglietto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//variabili
+		int età;
+		int kmViaggio;
+		Scanner scan = new Scanner(System.in);
+		
+		// Inserimento età
+		System.out.print("Inserisci la tua età: ");
+		età = scan.nextInt();
+		
+		// Inserimento km viaggio
+		System.out.print("Inserisci i km da percorrere: ");
+		kmViaggio = scan.nextInt();
+		
+		// Restituisci prezzo iniziale
+		float prezzoBiglietto = (float) (kmViaggio * 0.21);
+		System.out.print("Prezzo del biglietto : " + prezzoBiglietto);
+		
+ 
+	    
+		
+	}
 	}
 
-}
+
