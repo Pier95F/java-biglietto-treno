445c6eab980ff849d6dffe94eb6d471716c31a8e
Pier95F <forcinapierpaolo@gmail.com>
2022-06-08 10:08:36 +0200

    fixed correzione

--- src/jana60/calcolaBiglietto.java
+++ src/jana60/calcolaBiglietto.java
@@ -1,39 +1,46 @@
 package jana60;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class calcolaBiglietto {
 
 	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		DecimalFormat df = new DecimalFormat ("#0.00");
 
 		//variabili
 		int età;
 		int kmViaggio;
-		Scanner scan = new Scanner(System.in);
+		
 		
 		// Inserimento età
 		System.out.print("Inserisci la tua età: ");
-		età = scan.nextInt();
+		età = scanner.nextInt();
 		
 		// Inserimento km viaggio
 		System.out.print("Inserisci i km da percorrere: ");
-		kmViaggio = scan.nextInt();
+		kmViaggio = scanner.nextInt();
 		
 		// Restituisci prezzo iniziale
-		float prezzoBiglietto = (float) (kmViaggio * 0.21);
-		System.out.println ("Prezzo del biglietto : " + prezzoBiglietto);
-		
+		float prezzoBiglietto = (float) (kmViaggio * 0.21);	
+		System.out.println("Prezzo biglietto : " + df.format(prezzoBiglietto));
 	
 		// Applica sconto sul biglietto
 		if (età <18) {
 			float bigliettoUnder = (float) (prezzoBiglietto - prezzoBiglietto * 0.2);
-			System.out.printf ("Prezzo scontato : %.2f " , bigliettoUnder);
-		} else if (età >65) {
+			System.out.println("Hai diritto allo sconto minorenni");
+			System.out.println("Prezzo scontato : " + df.format(prezzoBiglietto));
+		} else if (età >=65) {
 			float bigliettoOver = (float) (prezzoBiglietto - prezzoBiglietto * 0.4);
-			System.out.printf ("Prezzo scontato : %.2f " , bigliettoOver);
+			System.out.println("Hai diritto allo sconto over65");
+			System.out.println("Prezzo scontato : " + df.format(prezzoBiglietto));
 		}
 		
- 
+		scanner.close();
+		
 	}
 	}
 
